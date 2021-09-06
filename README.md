cf75cceeb9deb25906b717e15652673acd71a54f
André Martins <andresmartins910@gmail.com>
2021-09-06 15:11:33 -0300

    Tarefa finalizada.

--- README.md
+++ README.md
@@ -23,23 +23,23 @@ Antes de começar a desenvolver suas funções de resolução dos Katas, crie a 
 
 **Suas funções devem utilizar return para retornar os resultados solicitados**
 
-1.  Exibir os números de 1 a 25: _(1, 2, 3, …, 24, 25)_
-2.  Exibir os números de 25 a 1: _(25, 24, 23, …, 2, 1)_
-3.  Exibir os números de -1 a -25: _(-1, -2, -3, …, -24, -25)_
-4.  Exibir os números de -25 a -1: _(-25, -24, -23, …, -2, -1)_
-5.  Exibir os números ímpares de 25 a -25: _(25, 23, 21, …, -23, -25)_
-6.  Exibir os números divisíveis por 3 até o 100: _(3, 6, 9, …, 96, 99)_
-7.  Exibir os números divisíveis por 7 até o 100: _(7, 14, 21, …, 91, 98)_
-8.  Exibir os números divisíveis por 3 e os números divisíveis por 7 regressivamente a partir do 100: _(99, 98, 96, 93, 91, …, 14, 12, 9, 7, 6, 3)_
-9.  Exibir os números ímpares divisíveis por 5 até o 100: _(5, 15, 25, …, 85, 95)_
-10. Exibir os 20 elementos de sampleArray. _(469, 755, 244, …, 940, 472)_
-11. Exibir todos os números pares contidos em sampleArray. _(244, 758, 450, …, 940, 472)_
-12. Exibir todos os números ímpares contidos em sampleArray. _(469, 755, 245, …, 179, 535)_
-13. Exibir os números divisíveis por 8 em sampleArray: _(712, 456, …, 472)_
-14. Exibir o quadrado de cada elemento de sampleArray. _(219961, 570025, …, 222784)_
-15. Exibir a soma de todos os números de 1 a 20.
-16. Exibir a soma de todos os elementos de sampleArray.
-17. Exibir o menor elemento de sampleArray.
+1.  Exibir os números de 1 a 25: _(1, 2, 3, …, 24, 25)_[DONE]
+2.  Exibir os números de 25 a 1: _(25, 24, 23, …, 2, 1)_[DONE]
+3.  Exibir os números de -1 a -25: _(-1, -2, -3, …, -24, -25)_[DONE]
+4.  Exibir os números de -25 a -1: _(-25, -24, -23, …, -2, -1)_[DONE]
+5.  Exibir os números ímpares de 25 a -25: _(25, 23, 21, …, -23, -25)_[DONE]
+6.  Exibir os números divisíveis por 3 até o 100: _(3, 6, 9, …, 96, 99)_[DONE]
+7.  Exibir os números divisíveis por 7 até o 100: _(7, 14, 21, …, 91, 98)_[DONE]
+8.  Exibir os números divisíveis por 3 e os números divisíveis por 7 regressivamente a partir do 100: _(99, 98, 96, 93, 91, …, 14, 12, 9, 7, 6, 3)_[DONE]
+9.  Exibir os números ímpares divisíveis por 5 até o 100: _(5, 15, 25, …, 85, 95)_[DONE]
+10. Exibir os 20 elementos de sampleArray. _(469, 755, 244, …, 940, 472) [DONE]
+11. Exibir todos os números pares contidos em sampleArray. _(244, 758, 450, …, 940, 472)_[DONE]
+12. Exibir todos os números ímpares contidos em sampleArray. _(469, 755, 245, …, 179, 535)_[DONE]
+13. Exibir os números divisíveis por 8 em sampleArray: _(712, 456, …, 472)_[DONE]
+14. Exibir o quadrado de cada elemento de sampleArray. _(219961, 570025, …, 222784) [DONE]
+15. Exibir a soma de todos os números de 1 a 20. [DONE]
+16. Exibir a soma de todos os elementos de sampleArray. [DONE]
+17. Exibir o menor elemento de sampleArray. 
 18. Exibir o maior elemento de sampleArray.
 
 ### Desafio Extra:
--- katas3.js
+++ katas3.js
@@ -1,76 +1,433 @@
 const sampleArray = [469, 755, 244, 245, 758, 450, 302, 20, 712, 71, 456, 21, 398, 339, 882, 848, 179, 535, 940, 472];
 
+// function - show results
+function showResults(x) {
+    const getList = document.getElementById('katas') // select element 'katas'
+    getList.appendChild(x) // append item to getList
+}
+// end
+
+
+
+// function - title
+function showTitle(titleText) {
+    const title = document.createElement('h2')
+    title.innerText = titleText
+    
+    const section = document.getElementById('katas')
+    section.appendChild(title)
+}
+// end
+
+
+
+// 1 to 25
 function kata1() {
-    // implemente o código do kata 1 aqui
+    let numArray = []
+    for (let i = 1; i <= 25; i++) {
+        numArray.push(i)
+    }
+
+    const kata1 = document.createElement('p')
+    kata1.innerText = numArray
+
+    showResults(kata1)
+
+    return numArray
 }
 
+showTitle('Kata 1')
+kata1()
+// 1 to 25 end
+
+
+
+// 25 to 1
 function kata2() {
-    // implemente o código do kata 2 aqui
+    let numArray = []
+    for (let i = 25; i >= 1; i--) {
+        numArray.push(i)
+    }
+
+    const kata2 = document.createElement('p')
+    kata2.innerText = numArray
+
+    showResults(kata2)
+
+    return numArray
 }
 
+showTitle('Kata 2')
+kata2()
+// 25 to 1 end
+
+
+
+// -1 to -25
 function kata3() {
-    // implemente o código do kata 3 aqui
+    let numArray = []
+    for (let i = -1; i >= -25; i--) {
+        numArray.push(i)
+    }
+
+    const kata3 = document.createElement('p')
+    kata3.innerText = numArray
+    
+    showResults(kata3)
+
+    return numArray
 }
 
+showTitle('Kata 3')
+kata3()
+//  -1 to -25 end
+
+
+
+// -25 to -1
 function kata4() {
-    // implemente o código do kata 4 aqui
+    let numArray = []
+    for (let i = -25; i <= -1; i++) {
+        numArray.push(i)
+    }
+
+    const kata4 = document.createElement('p')
+    kata4.innerText = numArray
+
+    showResults(kata4)
+
+    return numArray
 }
 
+showTitle('Kata 4')
+kata4()
+// -25 to -1 end
+
+
+
+// 25 to -25 odd numbers
 function kata5() {
-    // implemente o código do kata 5 aqui
+    let numArray = []
+    for (let i = 25; i >= -25; i--) {
+        if (i % 2 !== 0) {
+            numArray.push(i) 
+        }
+    }
+
+    const kata5 = document.createElement('p')
+    kata5.innerText = numArray
+
+    showResults(kata5)
+
+    return numArray
 }
 
+showTitle('Kata 5')
+kata5()
+// 25 to -25 odd numbers end
+
+
+
+// divisible by 3
 function kata6() {
-    // implemente o código do kata 6 aqui
+    let numArray = []
+    for (let i = 3; i <= 100; i++) {
+        if (i % 3 === 0) {
+            numArray.push(i) 
+        }
+    }
+
+    const kata6 = document.createElement('p')
+    kata6.innerText = numArray
+
+    showResults(kata6)
+
+    return numArray
 }
 
+showTitle('Kata 6')
+kata6()
+// divisible by 3 end
+
+
+
+// divisible by 7
 function kata7() {
-    // implemente o código do kata 7 aqui
+    let numArray = []
+    for (let i = 7; i <= 100; i++) {
+        if (i % 7 === 0) {
+            numArray.push(i) 
+        }
+    }
+
+    const kata7 = document.createElement('p')
+    kata7.innerText = numArray
+
+    showResults(kata7)
+
+    return numArray
 }
 
+showTitle('Kata 7')
+kata7()
+// divisible by 7 end
+
+
+
+// divisible by 3 and 7 regressively 
 function kata8() {
-    // implemente o código do kata 8 aqui
+    let numArray = []
+    for (let i = 100; i >= 3; i--) {
+        if (i % 3 === 0 || i % 7 === 0) {
+            numArray.push(i)
+        }
+    }
+
+    const kata8 = document.createElement('p')
+    kata8.innerText = numArray
+
+    showResults(kata8)
+
+    return numArray
 }
 
+showTitle('Kata 8')
+kata8()
+// divisible by 3 and 7 regressively end 
+
+
+
+// odd numbers divisible by 5
 function kata9() {
-    // implemente o código do kata 9 aqui
+    let numArray = []
+    for (let i = 5; i <= 100; i++) {
+        if (i % 5 === 0 && i % 2 !== 0) {
+            numArray.push(i)
+        }
+    }
+
+    const kata9 = document.createElement('p')
+    kata9.innerText = numArray
+
+    showResults(kata9)
+
+    return numArray
 }
 
+showTitle('Kata 9')
+kata9()
+// odd numbers divisible by 5 end
+
+
+
+// show sampleArray elements
 function kata10() {
-    // implemente o código do kata 10 aqui
+    const sampleArrayDupe = sampleArray
+    
+    const kata10 = document.createElement('p')
+    kata10.innerText = sampleArrayDupe
+
+    showResults(kata10)
+
+    return sampleArrayDupe
 }
 
+showTitle('Kata 10')
+kata10()
+// show sampleArray elements end
+
+
+
+// show even sampleArray elements
 function kata11() {
-    // implemente o código do kata 11 aqui
+    const sampleArrayDupe = sampleArray
+    const sampleArrayEven = []
+
+    for (let i = 0; i < sampleArrayDupe.length; i++) {
+        if (sampleArrayDupe[i] % 2 === 0) {
+            sampleArrayEven.push(sampleArrayDupe[i])
+        }
+    }
+
+    const kata11 = document.createElement('p')
+    kata11.innerText = sampleArrayEven
+
+    showResults(kata11)
+
+    return sampleArrayEven
 }
 
+showTitle('Kata 11')
+kata11()
+// show even sampleArray elements end
+
+
+
+// show odd sampleArray elements
 function kata12() {
-    // implemente o código do kata 12 aqui
+    const sampleArrayDupe = sampleArray
+    const sampleArrayOdd = []
+
+    for (let i = 0; i < sampleArrayDupe.length; i++) {
+        if (sampleArrayDupe[i] % 2 !== 0) {
+            sampleArrayOdd.push(sampleArrayDupe[i])
+        }
+    }
+
+    const kata12 = document.createElement('p')
+    kata12.innerText = sampleArrayOdd
+
+    showResults(kata12)
+
+    return sampleArrayOdd
 }
 
+showTitle('Kata 12')
+kata12()
+// show odd sampleArray elements end
+
+
+
+// divisible by 8
 function kata13() {
-    // implemente o código do kata 13 aqui
+    const sampleArrayDupe = sampleArray
+    const sampleArrayDivisibleByEigth = []
+
+    for (let i = 0; i < sampleArrayDupe.length; i++) {
+        if (sampleArrayDupe[i] % 8 === 0) {
+            sampleArrayDivisibleByEigth.push(sampleArrayDupe[i])
+        }
+    }
+
+    const kata13 = document.createElement('p')
+    kata13.innerText = sampleArrayDivisibleByEigth
+
+    showResults(kata13)
+
+    return sampleArrayDivisibleByEigth
 }
 
+showTitle('Kata 13')
+kata13()
+// divisible by 8 end
+
+
+
+// sampleArray squared
 function kata14() {
-    // implemente o código do kata 14 aqui
+    const sampleArrayDupe = sampleArray
+    const sampleArraySquared = []
+
+    for (let i = 0; i < sampleArrayDupe.length; i++) {
+        sampleArraySquared.push(Math.pow(sampleArrayDupe[i], 2))
+    }
+
+    const kata14 = document.createElement('p')
+    kata14.innerText = sampleArraySquared
+
+    showResults(kata14)
+
+    return sampleArraySquared
 }
 
+showTitle('Kata 14')
+kata14()
+// sampleArray squared end
+
+
+
+// 1 to 20 - sum up
 function kata15() {
-    // implemente o código do kata 15 aqui
+    let oneToTwentySum = 0
+    for (let i = 1; i <= 20; i++) {
+        oneToTwentySum += i
+    }
+
+    const kata15 = document.createElement('p')
+    kata15.innerText = oneToTwentySum
+
+    showResults(kata15)
+
+    return oneToTwentySum
 }
 
+showTitle('Kata 15')
+kata15()
+// 1 to 20 - sum up end
+
+
+
+// sampleArray sum up
 function kata16() {
-    // implemente o código do kata 16 aqui
+    const sampleArrayDupe = sampleArray
+    let sampleArraySum = 0
+
+    for (let i = 0; i < sampleArrayDupe.length; i++) {
+        sampleArraySum += sampleArrayDupe[i]
+    }
+
+    const kata16 = document.createElement('p')
+    kata16.innerText = sampleArraySum
+
+    showResults(kata16)
+
+    return sampleArraySum
 }
 
+showTitle('Kata 16')
+kata16()
+// sampleArray sum up end
+
+
+
+// smallest number
 function kata17() {
-    // implemente o código do kata 17 aqui
+    const sampleArrayDupe = sampleArray
+    let smallestNumber = sampleArrayDupe[0]
+
+    for (let i = 0; i < sampleArrayDupe.length; i++) {
+        if (sampleArrayDupe[i] < smallestNumber) {
+            smallestNumber = sampleArrayDupe[i]
+        }
+    }
+
+    const kata17 = document.createElement('p')
+    kata17.innerText = smallestNumber
+
+    showResults(kata17)
+
+    return smallestNumber
 }
 
+showTitle('Kata 17')
+kata17()
+// smallest number end
+
+
+// biggest number
 function kata18() {
-    // implemente o código do kata 18 aqui
+    const sampleArrayDupe = sampleArray
+    let biggestNumber = sampleArrayDupe[0]
+
+    for (let i = 0; i < sampleArrayDupe.length; i++) {
+        if (sampleArrayDupe[i] > biggestNumber) {
+            biggestNumber = sampleArrayDupe[i]
+        }
+    }
+
+    const kata18 = document.createElement('p')
+    kata18.innerText = biggestNumber
+
+    showResults(kata18)
+
+    return smallestNumber
 }
+
+showTitle('Kata 18')
+kata18()
+// biggest number end
+
+
 
 /**
  * Daqui em diante são os bônus, por sua conta e risco
